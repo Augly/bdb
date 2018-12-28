91438bc6e326c8c6f39dadf6e3ef29ba83ea09d5
wyq <1612803019@qq.com>
2018-12-28 17:35:53 +0800

    销售端 上传 （图片还没加）

---
+++ 销售端/pages/sale/index/index.js
@@ -0,0 +1,110 @@
+// pages/sale/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    mask:true,
+  },
+  // 诊所统计
+  go_clinic() {
+    wx.navigateTo({
+      url: '/pages/sale/clinic/clinic',
+    })
+  },
+  // 流量统计
+  go_flow(){
+    wx.navigateTo({
+      url: '/pages/sale/flow/flow',
+    })
+  },
+  // 销量统计
+  go_saleinfo(){
+    wx.navigateTo({
+      url: '/pages/sale/salelist/salelist',
+    })
+  },
+  // 消息
+  go_message() {
+    wx.navigateTo({
+      url: '/pages/sale/message/message',
+    })
+  },
+  go_my() {
+    wx.navigateTo({
+      url: '/pages/sale/my/my',
+    })
+  },
+  go_date(){
+    wx.navigateTo({
+      url: '/pages/sale/date/date',
+    })
+  },
+  del_submit(){
+    wx.navigateTo({
+      url: '/pages/sale/my/my',
+    })
+  },
+  close_code(){
+    this.setData({
+      mask:false
+    })
+  },
+ 
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
