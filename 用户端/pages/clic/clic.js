7490a2d427a490dbe5c3c9e2e6c2d91a1bd895c7
Augly <17633369350@163.com>
2019-01-08 09:59:26 +0800

    更新搜索方式

--- 用户端/pages/clic/clic.js
+++ 用户端/pages/clic/clic.js
@@ -1,7 +1,6 @@
 // pages/userport/search/search.js
 const app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -16,19 +15,27 @@ Page({
     citylist: [],
     otherList: [],
     otherIndex: 0,
-    medicine_list: [],
+    medicine_list: []
   },
   sale_medicine(e) {
     wx.navigateTo({
-      url: `/pages/userport/introduce/introduce?hosId=${e.currentTarget.dataset.id}`,
+      url: `/pages/userport/introduce/introduce?hosId=${
+        e.currentTarget.dataset.id
+      }`
     })
+  },
+  suresearch() {
+    this.setData({
+      disId: ''
+    })
+    this.getList()
   },
   //选择二级
   changes(e) {
     this.setData({
       disId: e.currentTarget.dataset.id,
       otherIndex: e.currentTarget.dataset.index,
-      choose: false,
+      choose: false
     })
     this.getList()
   },
@@ -36,12 +43,15 @@ Page({
   citySelect(e) {
     this.setData({
       cityIndex: e.currentTarget.dataset.index,
+      disId: e.currentTarget.dataset.id,
+      otherList: this.data.citylist[e.currentTarget.dataset.index].second
     })
+    this.getList()
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
       id: options.id
     })
@@ -49,40 +59,50 @@ Page({
       success: res => {
         console.log(res.windowWidth)
         this.setData({
-          myHeight: res.windowHeight - (res.windowWidth / 750 * 188)
+          myHeight: res.windowHeight - (res.windowWidth / 750) * 188
         })
       },
-      fail: function (res) { },
-      complete: function (res) { },
+      fail: function(res) {},
+      complete: function(res) {}
     })
     this.getdis()
     this.getList()
   },
   //获取区域
   getdis() {
-    app.config.ajax('POST', {
-      token: app.globalData.user_token
-    }, 'user/hospital/district_list', res => {
-      console.log(res)
-      this.setData({
-        citylist: res.data.data,
-        otherList: res.data.data[0].second
-      })
-    })
+    app.config.ajax(
+      'POST',
+      {
+        token: app.globalData.user_token
+      },
+      'user/hospital/district_list',
+      res => {
+        console.log(res)
+        this.setData({
+          citylist: res.data.data,
+          otherList: res.data.data[0].second
+        })
+      }
+    )
   },
   //获取诊所列表
   getList() {
-    app.config.ajax('POST', {
-      token: app.globalData.user_token,
-      key: '',
-      longitude: app.globalData.user_Location.longitude,
-      latitude: app.globalData.user_Location.latitude,
-      district_id: this.data.disId
-    }, 'user/hospital/hospital_list', res => {
-      this.setData({
-        medicine_list: res.data.data
-      })
-    })
+    app.config.ajax(
+      'POST',
+      {
+        token: app.globalData.user_token,
+        key: '',
+        longitude: app.globalData.user_Location.longitude,
+        latitude: app.globalData.user_Location.latitude,
+        district_id: this.data.disId
+      },
+      'user/hospital/hospital_list',
+      res => {
+        this.setData({
+          medicine_list: res.data.data
+        })
+      }
+    )
   },
   getValue(e) {
     this.setData({
@@ -99,60 +119,46 @@ Page({
     this.setData({
       disId: '',
       cityIndex: -1,
-      otherIndex: -1,
+      otherIndex: -1
     })
     this.getList()
     this.setData({
       choose: false,
-      choosed: false,
+      choosed: false
     })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
+  onShareAppMessage: function() {}
 })
--- 用户端/pages/userport/search/search.js
+++ 用户端/pages/userport/search/search.js
@@ -1,39 +1,46 @@
 // pages/userport/search/search.js
-const app=getApp()
+const app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     imgUrl: app.ImageHost,
-    choosed:false,
-    choose:false,
-    key:'',
-    id:'',
-    disId:'',
-    cityIndex:-1,
-    citylist:[],
-    otherList:[],
-    otherIndex:-1,
-    medicine_list: [],
+    choosed: false,
+    choose: false,
+    key: '',
+    id: '',
+    disId: '',
+    cityIndex: -1,
+    citylist: [],
+    otherList: [],
+    otherIndex: -1,
+    medicine_list: []
   },
-  sale_medicine(e){
+  sale_medicine(e) {
     wx.navigateTo({
-      url: `/pages/userport/salemedicine/salemedicine?medicine=${this.data.id}&hosId=${e.currentTarget.dataset.id}`,
+      url: `/pages/userport/salemedicine/salemedicine?medicine=${
+        this.data.id
+      }&hosId=${e.currentTarget.dataset.id}`
     })
   },
+  suresearch() {
+    this.setData({
+      disId: ''
+    })
+    this.getList()
+  },
   //选择二级
-  changes(e){
+  changes(e) {
     this.setData({
       disId: e.currentTarget.dataset.id,
-      otherIndex:e.currentTarget.dataset.index,
-      choose:false,
+      otherIndex: e.currentTarget.dataset.index,
+      choose: false
     })
     this.getList()
   },
   //选择一级
-  citySelect(e){
+  citySelect(e) {
     this.setData({
       cityIndex: e.currentTarget.dataset.index,
       disId: e.currentTarget.dataset.id,
@@ -44,118 +51,114 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
-      id:options.id
+      id: options.id
     })
     wx.getSystemInfo({
-      success: res=>{
+      success: res => {
         console.log(res.windowWidth)
         this.setData({
-          myHeight: res.windowHeight - (res.windowWidth/750*190)
+          myHeight: res.windowHeight - (res.windowWidth / 750) * 190
         })
       },
       fail: function(res) {},
-      complete: function(res) {},
+      complete: function(res) {}
     })
     this.getdis()
     this.getList()
   },
   //获取区域
-  getdis(){
-    app.config.ajax('POST', {
-      token: app.globalData.user_token
-    }, 'user/hospital/district_list', res => {
-      console.log(res)
-      this.setData({
-        citylist:res.data.data,
-        otherList: res.data.data[0].second
-      })
-    })  
+  getdis() {
+    app.config.ajax(
+      'POST',
+      {
+        token: app.globalData.user_token
+      },
+      'user/hospital/district_list',
+      res => {
+        console.log(res)
+        this.setData({
+          citylist: res.data.data,
+          otherList: res.data.data[0].second
+        })
+      }
+    )
   },
   //获取诊所列表
-  getList(){
-    app.config.ajax('POST', {
-      token: app.globalData.user_token,
-      key: this.data.key,
-      longitude: app.globalData.user_Location.longitude,
-      latitude: app.globalData.user_Location.latitude,
-      district_id: this.data.disId
-    }, 'user/hospital/hospital_list', res => {
-      this.setData({
-        medicine_list:res.data.data
-      })
-    })  
+  getList() {
+    app.config.ajax(
+      'POST',
+      {
+        token: app.globalData.user_token,
+        key: this.data.key,
+        longitude: app.globalData.user_Location.longitude,
+        latitude: app.globalData.user_Location.latitude,
+        district_id: this.data.disId
+      },
+      'user/hospital/hospital_list',
+      res => {
+        this.setData({
+          medicine_list: res.data.data
+        })
+      }
+    )
   },
-  getValue(e){
+  getValue(e) {
     this.setData({
-      key:e.detail.value
+      key: e.detail.value
     })
   },
-  showMask(){
+  showMask() {
     this.setData({
       choosed: true,
-      choose:!this.data.choose
+      choose: !this.data.choose
     })
   },
-  hideMask(){
+  hideMask() {
     this.setData({
-      disId:'',
+      disId: '',
       cityIndex: -1,
-      otherIndex: -1,
+      otherIndex: -1
     })
     this.getList()
     this.setData({
       choose: false,
-      choosed:false,
+      choosed: false
     })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
+  onShareAppMessage: function() {}
 })
