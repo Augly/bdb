5b1fb12097bf672f0190fa2701f74d953d7a265e
Augly <17633369350@163.com>
2019-01-10 16:14:38 +0800

    注释点图片验证码

--- 用户端/pages/userport/index/index.js
+++ 用户端/pages/userport/index/index.js
@@ -1,153 +1,172 @@
 // pages/userport/index/index.js
 const app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    banner_list: [
-      '/images/14.png',
-      '/images/14.png',
-      '/images/14.png'
-    ],
+    banner_list: ['/images/14.png', '/images/14.png', '/images/14.png'],
     imgUrl: app.ImageHost,
     goodsList: [],
     history: [],
     unread: 0,
     mask: false
   },
-  sure() {
-    wx.switchTab({
-      url: '/pages/userport/my/my',
-      success: function (res) {},
-      fail: function (res) {},
-      complete: function (res) {},
-    })
+  sure(e) {
+    console.log(e)
+    // wx.switchTab({
+    //   url: '/pages/userport/my/my',
+    //   success: function (res) {},
+    //   fail: function (res) {},
+    //   complete: function (res) {},
+    // })
   },
   toyyed(e) {
     wx.navigateTo({
-      url: '/pages/userport/finish/finish?type=finish&id='+e.currentTarget.dataset.id,
-      success: function (res) {},
-      fail: function (res) {},
-      complete: function (res) {},
+      url:
+        '/pages/userport/finish/finish?type=finish&id=' +
+        e.currentTarget.dataset.id,
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {}
     })
   },
   toclic(e) {
     wx.navigateTo({
       url: '/pages/userport/search/search?id=' + e.currentTarget.dataset.id,
-      success: function (res) {},
-      fail: function (res) {},
-      complete: function (res) {},
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {}
     })
   },
   // 历史列表
   go_list() {
     wx.navigateTo({
-      url: '/pages/userport/mydata/mydata?type=finish',
+      url: '/pages/userport/mydata/mydata?type=finish'
     })
   },
   tores() {
     wx.navigateTo({
       url: '/pages/userport/salemedicine/salemedicine',
-      success: function (res) {},
-      fail: function (res) {},
-      complete: function (res) {},
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {}
     })
   },
   // 跳转我的
   my() {
     wx.navigateTo({
-      url: '/pages/userport/my/my',
+      url: '/pages/userport/my/my'
     })
   },
   //获取个人信息
   getUserInfo() {
-    app.config.ajax('POST', {
-      token: app.globalData.user_token,
-    }, 'user/user/user_info', res => {
-      this.setData({
-        mask: res.data.data.user_isrevised == 1?true:false,
-        userInfo: res.data.data
-      })
-    })
+    app.config.ajax(
+      'POST',
+      {
+        token: app.globalData.user_token
+      },
+      'user/user/user_info',
+      res => {
+        this.setData({
+          mask: res.data.data.user_isrevised == 1 ? true : false,
+          userInfo: res.data.data
+        })
+      }
+    )
   },
   // 跳转更多药品
   more_medicine() {
     wx.navigateTo({
-      url: '/pages/userport/medicinelist/medicinelist',
+      url: '/pages/userport/medicinelist/medicinelist'
     })
   },
   // 跳转医务中心
   tointroduce() {
     wx.navigateTo({
-      url: '/pages/userport/introduce/introduce',
+      url: '/pages/userport/introduce/introduce'
     })
   },
   // 跳转message
   toMessges() {
     wx.navigateTo({
-      url: '/pages/userport/message/message',
+      url: '/pages/userport/message/message'
     })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
-  },
+  onLoad: function(options) {},
   //获取首页轮播图
   gitBanner() {
-    app.config.ajax('POST', {
-      token: app.globalData.user_token
-    }, 'user/index/banner_list', res => {
-      this.setData({
-        banner_list: res.data.data
-      })
-    })
+    app.config.ajax(
+      'POST',
+      {
+        token: app.globalData.user_token
+      },
+      'user/index/banner_list',
+      res => {
+        this.setData({
+          banner_list: res.data.data
+        })
+      }
+    )
   },
   //获取我的预约
   gitHistorySubscribe() {
-    app.config.ajax('POST', {
-      token: app.globalData.user_token
-    }, 'user/index/history_subscribe', res => {
-      this.setData({
-        history: res.data.data
-      })
-    })
+    app.config.ajax(
+      'POST',
+      {
+        token: app.globalData.user_token
+      },
+      'user/index/history_subscribe',
+      res => {
+        this.setData({
+          history: res.data.data
+        })
+      }
+    )
   },
   //获取首页药品
   gitGoods() {
-    app.config.ajax('POST', {
-      token: app.globalData.user_token
-    }, 'user/index/goods_list', res => {
-      this.setData({
-        goodsList: res.data.data
-      })
-    })
+    app.config.ajax(
+      'POST',
+      {
+        token: app.globalData.user_token
+      },
+      'user/index/goods_list',
+      res => {
+        this.setData({
+          goodsList: res.data.data
+        })
+      }
+    )
   },
   //获取未读消息
   gitUnread() {
-    app.config.ajax('POST', {
-      token: app.globalData.user_token
-    }, 'user/index/unread_count', res => {
-      this.setData({
-        unread: res.data.data.unread
-      })
-    })
+    app.config.ajax(
+      'POST',
+      {
+        token: app.globalData.user_token
+      },
+      'user/index/unread_count',
+      res => {
+        this.setData({
+          unread: res.data.data.unread
+        })
+      }
+    )
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     wx.getStorage({
       key: 'user_token',
       success: res => {
@@ -162,48 +181,37 @@ Page({
         app.globalData.user_token = ''
         wx.redirectTo({
           url: '/pages/userport/login/login',
-          success: function (res) { },
-          fail: function (res) { },
-          complete: function (res) { },
+          success: function(res) {},
+          fail: function(res) {},
+          complete: function(res) {}
         })
       },
-      complete: function (res) { },
+      complete: function(res) {}
     })
-    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
+  onShareAppMessage: function() {}
 })
--- 用户端/pages/userport/login/login.js
+++ 用户端/pages/userport/login/login.js
@@ -13,7 +13,7 @@ Page({
     code: '', //验证码
     picCode: '', //图片验证码
     erroCode: false,
-    text: '',
+    text: ''
   },
   close_code() {
     //关闭图片验证
@@ -40,22 +40,22 @@ Page({
     })
   },
   randomNum(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min) + min)
   },
   /**生成一个随机色**/
   randomColor(min, max) {
-    var r = this.randomNum(min, max);
-    var g = this.randomNum(min, max);
-    var b = this.randomNum(min, max);
-    return "rgb(" + r + "," + g + "," + b + ")";
+    var r = this.randomNum(min, max)
+    var g = this.randomNum(min, max)
+    var b = this.randomNum(min, max)
+    return 'rgb(' + r + ',' + g + ',' + b + ')'
   },
 
   /**绘制验证码图片**/
   drawPic(that) {
-    var ctx = wx.createCanvasContext('canvas');
+    var ctx = wx.createCanvasContext('canvas')
     /**绘制背景色**/
-    ctx.fillStyle = this.randomColor(180, 240); //颜色若太深可能导致看不清
-    var s, t;
+    ctx.fillStyle = this.randomColor(180, 240) //颜色若太深可能导致看不清
+    var s, t
     app.config.remW(200, res => {
       s = res
     })
@@ -64,95 +64,100 @@ Page({
     })
     ctx.fillRect(0, 0, s, t)
     /**绘制文字**/
-    var arr;
-    var text = '';
-    var str = 'qwertyuiopasdfghjklzxcvbnm123456789';
+    var arr
+    var text = ''
+    var str = 'qwertyuiopasdfghjklzxcvbnm123456789'
     for (var i = 0; i < 4; i++) {
-      var txt = str[this.randomNum(0, str.length)];
-      ctx.fillStyle = this.randomColor(50, 160); //随机生成字体颜色
-      ctx.font = this.randomNum(25, 30) + 'px SimHei'; //随机生成字体大小
-      var x = 5 + i * 20;
-      var y = this.randomNum(20, t / 2);
-      var deg = this.randomNum(-20, 20);
+      var txt = str[this.randomNum(0, str.length)]
+      ctx.fillStyle = this.randomColor(50, 160) //随机生成字体颜色
+      ctx.font = this.randomNum(25, 30) + 'px SimHei' //随机生成字体大小
+      var x = 5 + i * 20
+      var y = this.randomNum(20, t / 2)
+      var deg = this.randomNum(-20, 20)
       //修改坐标原点和旋转角度
-      ctx.translate(x, y);
-      ctx.rotate(deg * Math.PI / 180);
-      ctx.fillText(txt, 10, t / 4);
-      text = text + txt;
+      ctx.translate(x, y)
+      ctx.rotate((deg * Math.PI) / 180)
+      ctx.fillText(txt, 10, t / 4)
+      text = text + txt
       //恢复坐标原点和旋转角度
-      ctx.rotate(-deg * Math.PI / 180);
-      ctx.translate(-x, -y);
+      ctx.rotate((-deg * Math.PI) / 180)
+      ctx.translate(-x, -y)
     }
     /**绘制干扰线**/
     for (var i = 0; i < 4; i++) {
-      ctx.strokeStyle = this.randomColor(40, 180);
-      ctx.beginPath();
-      ctx.moveTo(this.randomNum(0, s), this.randomNum(0, t));
-      ctx.lineTo(this.randomNum(0, s), this.randomNum(0, t));
-      ctx.stroke();
+      ctx.strokeStyle = this.randomColor(40, 180)
+      ctx.beginPath()
+      ctx.moveTo(this.randomNum(0, s), this.randomNum(0, t))
+      ctx.lineTo(this.randomNum(0, s), this.randomNum(0, t))
+      ctx.stroke()
     }
     /**绘制干扰点**/
     for (var i = 0; i < 20; i++) {
-      ctx.fillStyle = this.randomColor(0, 255);
-      ctx.beginPath();
-      ctx.arc(this.randomNum(0, s), this.randomNum(0, t), 1, 0, 2 * Math.PI);
-      ctx.fill();
+      ctx.fillStyle = this.randomColor(0, 255)
+      ctx.beginPath()
+      ctx.arc(this.randomNum(0, s), this.randomNum(0, t), 1, 0, 2 * Math.PI)
+      ctx.fill()
     }
-    ctx.draw(false, function () {
+    ctx.draw(false, function() {
       console.log(text)
       that.setData({
         text: text
       })
-    });
+    })
   },
-  change: function () {
-    var that = this;
-    this.drawPic(that);
+  change: function() {
+    var that = this
+    this.drawPic(that)
   },
   index_page() {
-    if (this.data.picCode != this.data.text) {
-      this.setData({
-        erroCode: true
-      })
-      this.change()
-      return false
-    } else {
-      this.setData({
-        erroCode: false
-      })
-    }
+    // if (this.data.picCode != this.data.text) {
+    //   this.setData({
+    //     erroCode: true
+    //   })
+    //   this.change()
+    //   return false
+    // } else {
+    //   this.setData({
+    //     erroCode: false
+    //   })
+    // }
     //验证码读秒并关闭图片验证
     if (this.data.codeStatua == '发送') {
-      app.config.ajax('POST', {
-        phone: this.data.phone
-      }, 'user/login/send_message', res => {
-        this.setData({
-          pic_code_wrap: !this.data.pic_code_wrap
-        })
-        this.setData({
-          codeStatua: 60,
-          code_tips: true,
-        })
-        let st = setInterval(() => {
-          let n = this.data.codeStatua--
-          if (n == 1) {
-            this.setData({
-              codeStatua: '发送'
-            })
-            clearInterval(st)
-          } else {
-            n--
-            this.setData({
-              codeStatua: n
-            })
-          }
-        }, 1000)
-      })
+      app.config.ajax(
+        'POST',
+        {
+          phone: this.data.phone
+        },
+        'user/login/send_message',
+        res => {
+          // this.setData({
+          //   pic_code_wrap: !this.data.pic_code_wrap
+          // })
+          this.setData({
+            codeStatua: 60,
+            code_tips: true
+          })
+          let st = setInterval(() => {
+            let n = this.data.codeStatua--
+            if (n == 1) {
+              this.setData({
+                codeStatua: '发送'
+              })
+              clearInterval(st)
+            } else {
+              n--
+              this.setData({
+                codeStatua: n
+              })
+            }
+          }, 1000)
+        }
+      )
     }
   },
   login_btn(res) {
     //登陆输入判断成功
-    if (!(/^1[34578]\d{9}$/.test(this.data.phone))) {
+    if (!/^1[34578]\d{9}$/.test(this.data.phone)) {
       app.config.mytoast('请输入正确的手机号!')
       return false
     }
@@ -160,100 +165,90 @@ Page({
       app.config.mytoast('请输入短信验证码!')
       return false
     }
-    app.config.ajax('POST', {
-      phone: this.data.phone,
-      code: this.data.code
-    }, 'user/login/user_login', res => {
-      app.globalData.user_token = res.data.data.user_token
-      wx.setStorage({
-        key: 'user_token',
-        data: res.data.data.user_token,
-        success: function (res) { 
-          wx.switchTab({
-            url: '/pages/userport/index/index',
-          })
-        },
-        fail: function (res) { },
-        complete: function (res) { },
-      })
-      
-    })
+    app.config.ajax(
+      'POST',
+      {
+        phone: this.data.phone,
+        code: this.data.code
+      },
+      'user/login/user_login',
+      res => {
+        app.globalData.user_token = res.data.data.user_token
+        wx.setStorage({
+          key: 'user_token',
+          data: res.data.data.user_token,
+          success: function(res) {
+            wx.switchTab({
+              url: '/pages/userport/index/index'
+            })
+          },
+          fail: function(res) {},
+          complete: function(res) {}
+        })
+      }
+    )
     // wx.switchTab({
     //   url: '/pages/userport/index/index',
     // })
   },
 
-  SendCode: function () {
+  SendCode: function() {
     //开启图片验证码验证
-    var that = this;
-    this.drawPic(that);
-    if (!(/^1[34578]\d{9}$/.test(this.data.phone))) {
+    var that = this
+    this.drawPic(that)
+    if (!/^1[34578]\d{9}$/.test(this.data.phone)) {
       //验证手机号
       app.config.mytoast('请输入正确的手机号!')
       return false
     }
 
     if (this.data.codeStatua == '发送') {
-      this.setData({
-        pic_code_wrap: true
-      })
+      // this.setData({
+      //   pic_code_wrap: true
+      // })
+      this.index_page()
     }
-
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    var that = this;
-    this.drawPic(that);
+  onShow: function() {
+    var that = this
+    this.drawPic(that)
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
+  onShareAppMessage: function() {}
 })
